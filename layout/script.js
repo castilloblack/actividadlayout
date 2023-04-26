5eee3baae053723f149306e426a205e73bc7486e
castilloblack <125686686+castilloblack@users.noreply.github.com>
2023-04-26 11:09:49 -0600

    Add files via upload

---
+++ layout/script.js
@@ -0,0 +1,26 @@
+ var o = {a:70};
+            var x = {a:80};
+            var y = {a:90};
+            var t = {a:120};
+            var p = {a:30};
+
+          var i;
+                for (i in o){ 
+                    document.write("hamburguesa veganas" + " : " + o [ i ] + "<br>");
+                }
+            var i;
+                for (i in x){ 
+                    document.write("hamburguesa con papas" + " : " + x [ i ] + "<br>");
+                }
+          var i;
+                for (i in y){ 
+                    document.write("hamburguesa con doble carne" + " : " + y [ i ] + "<br>");
+                }
+          var i;
+                for (i in t){ 
+                    document.write("VIP FOOD" + " : " + t [ i ] + "<br>");
+                }
+             var i;
+                for (i in p){ 
+                    document.write("VIP DRINKS" + " : " + p [ i ] + "<br>");
+                }
